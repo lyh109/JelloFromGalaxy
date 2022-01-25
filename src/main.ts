3132b52920e4ce929e316fe5671daa8b5c24504b
DESKTOP-Q3MPM7R\lionine <meekk1009@gmail.com>
2022-01-25 17:12:40 +0900

    초기 세팅

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import Phaser from 'phaser'
+import 'phaser/plugins/spine/dist/SpinePlugin'
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 960,
+    physics: {
+        default: 'matter',
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [],
+    plugins: {
+        scene: [
+            { key: 'SpinePlugin', plugin: window.SpinePlugin, mapping: 'spine'}
+        ]
+    }
+}
+
+export default new Phaser.Game(config)
---
+++ src/types/globals.d.ts
@@ -0,0 +1,12 @@
+declare interface Window
+{
+	SpinePlugin: any
+}
+
+interface TilesConfig
+{
+	type: string
+  	texture: string
+  	x: number
+  	y: number
+}
