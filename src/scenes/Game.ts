5c9ce51e516b4a46db26e95f4aeb442db3528e1b
DESKTOP-Q3MPM7R\lionine <meekk1009@gmail.com>
2022-01-26 13:46:03 +0900

    Game Scene 생성

---
+++ src/scenes/Game.ts
@@ -0,0 +1,37 @@
+import SceneKeys from "~/consts/SceneKeys"
+import TextureKeys from "~/consts/TextureKeys"
+
+export default class Game extends Phaser.Scene
+{
+    private background!: Phaser.GameObjects.TileSprite
+    private selectedCharacter!: string
+
+    constructor()
+    {
+        super(SceneKeys.Game)
+    }
+
+    /**
+	 * @param {{ character: string }} data 
+	 */
+
+    init(data)
+    {   
+        this.selectedCharacter = data.character
+    }
+
+    create()
+    {
+        this.cameras.main.fadeIn()
+
+        const width = this.scale.width
+        const height = this.scale.height
+
+        this.background = this.add.tileSprite(0, 0, width, height, TextureKeys.Background).setOrigin(0)
+    }
+
+    update(time: number, delta: number): void 
+    {
+        this.background.tilePositionY += 0.5
+    }
+}
