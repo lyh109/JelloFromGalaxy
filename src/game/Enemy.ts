fc2773f1d8ace541b63d83b42472f566d970b5bf
DESKTOP-Q3MPM7R\lionine <meekk1009@gmail.com>
2022-02-07 15:14:11 +0900

    ENEMY HP 추가

--- src/game/Enemy.ts
+++ src/game/Enemy.ts
@@ -1,23 +1,62 @@
+import TextureKeys from "~/consts/TextureKeys"
+
+const SHIP_BLUE_TEXTURE: string[] = [TextureKeys.SHIP_BLUE1, TextureKeys.SHIP_BLUE2, TextureKeys.SHIP_BLUE3, TextureKeys.SHIP_BLUE4]
+
 export default class Enemy extends Phaser.Physics.Matter.Image
 {
     isCollided: boolean = false
-    constructor(scene: Phaser.Scene, x: number, y: number, texture: string)
+    maxHP: number = 3
+    hp: number = 3
+
+    textures: string[] = SHIP_BLUE_TEXTURE
+
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string = TextureKeys.SHIP_BLUE1)
     {
         super(scene.matter.world, x, y, texture)
 
         scene.add.existing(this)
         this.scene = scene
         this.setIgnoreGravity(true)
+        this.setStatic(true)
     }
 
     update()
     {
-        this.y -= 5
+        this.y -= 1
     }
 
     despawn()
     {
-        console.log('despawn')
         this.isCollided = true
+    }
+
+    updateHP()
+    {
+        // 4 3 2 1 0
+        // 8 6 4 2 0
+        // 12 9 6 3 0
+        // 16 12 8 4 0
+
+        --this.hp
+
+        if(this.hp == 0)
+        {
+            this.despawn()
+            return
+        }
+        
+        if(this.hp <= this.maxHP * 1 / 4)
+        {
+            this.setTexture(this.textures[3])
+            SHIP_BLUE_TEXTURE
+        }
+        else if(this.hp <= this.maxHP * 2 / 4)
+        {
+            this.setTexture(this.textures[2])
+        }
+        else if(this.hp <= this.maxHP * 3 / 4)
+        {
+            this.setTexture(this.textures[1])
+        }
     }
 }
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -16,7 +16,10 @@ export default class Preloader extends Phaser.Scene
         this.load.image(TextureKeys.LOGO, 'bg/logo.png')
         this.load.image(TextureKeys.BLACK, 'bg/black.png')
 
-        this.load.image(TextureKeys.SHIP_BLUE, 'enemies/Ship_blue.png')
+        this.load.image(TextureKeys.SHIP_BLUE1, 'enemies/Ship_blue.png')
+        this.load.image(TextureKeys.SHIP_BLUE2, 'enemies/Ship_blue_Damaged1.png')
+        this.load.image(TextureKeys.SHIP_BLUE3, 'enemies/Ship_blue_Damaged2.png')
+        this.load.image(TextureKeys.SHIP_BLUE4, 'enemies/Ship_blue_Damaged3.png')
 
         this.load.image(TextureKeys.LASER1, 'laser/laserBlue1.png')
         this.load.image(TextureKeys.LASER2, 'laser/laserBlue2.png')
