b3526e72dbb6575b19d9ff6f2814bfd822880a22
DESKTOP-Q3MPM7R\lionine <meekk1009@gmail.com>
2022-02-14 10:06:16 +0900

    운석 클래스 멤버 함수 생성

--- src/game/Meteor.ts
+++ src/game/Meteor.ts
@@ -2,8 +2,56 @@ import TextureKeys from "~/consts/TextureKeys"
 
 export default class Meteor extends Phaser.Physics.Matter.Image
 {
-    constructor(scene: Phaser.Scene, x: number, y: number, texture: string = TextureKeys.SHIP_BLUE1)
+    private isCollided: boolean = false
+
+    private rotaitonSpeed: number = Phaser.Math.FloatBetween(-0.02, 0.02)
+    private xSpeed: number = Phaser.Math.FloatBetween(-1, 1)
+
+    constructor(scene: Phaser.Scene, x: number, y: number, minSize: number, maxSize: number)
     {
+        let texture: string = TextureKeys.METEOR1
+        if(Phaser.Math.Between(0, 1) == 0)
+        {
+            texture = TextureKeys.METEOR2
+        }
+
         super(scene.matter.world, x, y, texture)
+
+        scene.add.existing(this)
+        this.scene = scene
+        this.setIgnoreGravity(true)
+        this.setScale(Phaser.Math.FloatBetween(minSize, maxSize))
+
+        if(minSize >= 1)
+        {
+            this.xSpeed = 0
+        }
+    }
+
+    update()
+    {
+        this.x -= this.xSpeed
+        this.y -= 3
+        this.rotation += this.rotaitonSpeed
+
+        if(this.x <= 17 || this.x >= this.scene.scale.width - 17)
+        {
+            this.xSpeed *= -1
+        }
+
+        if(this.y < -this.height)
+        {
+            this.despawn()
+        }
+    }
+
+    IsCollided()
+    {
+        return this.isCollided
+    }
+
+    despawn()
+    {
+        this.isCollided = true
     }
 }
